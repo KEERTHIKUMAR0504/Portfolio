a01904769ab6971e04c8dbb8083f3b200f8d8b7c
KeerthikumarTS <tskeerthikumar0504@gmail.com>
2024-02-17 23:20:51 +0530

    fix: added links for github and linkedin

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin} from 'react-icons/ai'
 
 import styles from "./Profile.module.css";
 import { getImageUrl } from "../../utils";
@@ -9,11 +10,16 @@ export const Profile = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Keerthikumar</h1>
         <p className={styles.description}>
-        Passionate and Innovative Frontend Developer | Crafting Engaging User Experiences | Entry-Level Developer with a Strong Foundation in HTML, CSS, and JavaScript.
+        Passionate and Innovative Full-stack developer.
         </p>
-        <a href="mailto:tskeerthikumar0504@gmail.com" className={styles.contactBtn}>
-          Contact Me
+        <div>
+        <a href="https://www.linkedin.com/in/keerthikumar-ts" className={styles.contactBtn}>
+          <AiOutlineLinkedin/>
         </a>
+        <a href="https://github.com/KeerthikumarTS" className={styles.contactBtn}>
+          <AiOutlineGithub/>
+        </a>
+        </div>
       </div>
       <img
         src={getImageUrl("profile/profileImage.png")}
