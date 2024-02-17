fbc28f12a31bc2ffca775edaaa9e60fac944789d
KeerthikumarTS <tskeerthikumar0504@gmail.com>
2024-02-17 23:20:06 +0530

    fix: modified contact layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { MdPhone } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
 
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
@@ -7,20 +9,17 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2><br/>
+        <br/><h2>Contact</h2><br/>
         <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <MdEmail size={40}/>
           <a href="mailto:tskeerthikumar0504@gmail.com">tskeerthikumar0504@gmail.com</a>
         </li>
         <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="LinkedIn icon"
-          />
-          <a href="http://www.linkedin.com/in/keerthikumar-ts">linkedin.com/in/keerthikumar-ts</a>
+          <MdPhone size={40}/>
+          <a href="tel:+919360256033">+91 9360256033</a>
         </li>
       </ul>
     </footer>
